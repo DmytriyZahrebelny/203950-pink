552a4ad4aa6d7823e16da83f91c15f174fdb012b
Dima <zahrebelny@gmail.com>
2018-11-02 00:34:49 +0200

    decoration

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 "use strict";
-
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var webp = require("gulp-webp");
